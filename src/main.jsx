02534409521611308fcad02bc8fe9fd3a8bb89b5
kalyandeep7 <kalyandeep2001@gmail.com>
2026-04-08 21:21:56 +0530

    Add lazy loading for images

--- src/main.jsx
+++ src/main.jsx
@@ -5,3 +5,12 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Redux Provider makes store available to all components */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
