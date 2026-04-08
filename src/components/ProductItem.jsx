786c634d5e05c5af9d8094af770da98007c7e481
kalyandeep7 <kalyandeep2001@gmail.com>
2026-04-08 21:08:52 +0530

    Create ProductItem component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../redux/cartSlice';
+
+// Props: product object passed from ProductList
+const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
+
+  // Dispatch addToCart action with product data
+  const handleAddToCart = () => {
+    dispatch(addToCart({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      thumbnail: product.thumbnail,
+    }));
+  };
+
+  return (
+    <div className="product-card">
+      {/* Lazy loaded product image */}
+      <img src={product.thumbnail} alt={product.title} loading="lazy" />
+      <h3>{product.title}</h3>
+      <p className="price">${product.price}</p>
+      {/* Link to dynamic product detail route */}
+      <Link to={`/product/${product.id}`} className="btn btn-outline">View Details</Link>
+      <button className="btn btn-primary" onClick={handleAddToCart}>Add to Cart</button>
+    </div>
+  );
+};
