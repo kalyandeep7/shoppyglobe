69f15a53b9ee7a30ed284d86a7a23e856b4ab0dd
kalyandeep7 <kalyandeep2001@gmail.com>
2026-04-08 21:21:29 +0530

    Add React.lazy and Suspense for all components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// main.jsx - Entry point: wraps app with Redux Provider and React Router
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import './index.css';
